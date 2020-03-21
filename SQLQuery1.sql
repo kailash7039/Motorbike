85e410b514e165e061e525f5eb3cd641042b21ff
kailash7039 <61161609+kailash7039@users.noreply.github.com>
2020-03-21 11:49:54 +0530

    inital

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿Select A.*,dbo.getSizeName(2) as SizeNamee,'2' as SizeIDD,SizeData.Name,SizeData.Extention
+from tblProducts A
+Cross apply(
+	Select top 1 B.Name,Extention from  tblProductImages B where B.PID=A.PID
+) SizeData
+where A.PID=1
